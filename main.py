683548d85009fe99ab810ff9ea8457249b160791
lmarqu8 <lmarqu8@uic.edu>
2021-05-02 05:45:36 -0500

    edges function gives coordinates at the reachable points on the graph as str(x con) + str(y con) = reachable(str(x con)+ str(y con))

--- main.py
+++ main.py
@@ -1,33 +1,55 @@
 import sys
-
+import pprint
 from z3 import *
-import random
 from timeit import default_timer as timer
 import string
 
 db = string.ascii_lowercase + string.ascii_uppercase
 
 
+def edges(arr):
+    size = len(arr)
+
+    vertex = {}
+
+    for x in range(size):
+        for y in range(size):
+            tempSet = []
+            if x > 0: tempSet.append(str(x - 1) + str(y))
+            if y > 0: tempSet.append(str(x) + (str(y-1)))
+            if x < size-1: tempSet.append(str(x + 1) + str(y))
+            if y < size-1: tempSet.append(str(x) + str(y+1))
+            vertex[str(x)+str(y)] = tempSet
+
+    return vertex
+
+
+def graph2Sat(arr):
+    size = len(arr)
+    contains = getUniqueVar(arr)
+    numPins = len(contains)
+    numSpaces = size ** 2
+
+
 def variable(arr, contains):
-    vars = {}
+    variables = {}
     for i in range(len(arr)):
-        vars[i] = {}
+        variables[i] = {}
         for j in range(len(arr[0])):
-            vars[i][j] = {}
+            variables[i][j] = {}
             for k in contains:
-                coord = "("+str(i)+","+str(j)+")"
-                vars[i][j][k] = Bool(coord + k)
+                variables[i][j][k] = Bool(str(i) + str(j) + k)
 
     for i in range(len(arr1)):
         for j in range(len(arr1[0])):
             for k in contains:
-                print(vars[i][j][k], " ", end='')
+                print(variables[i][j][k], " ", end='')
             print()
         print()
-    return vars
+    return variables
 
 
-def _pathFinder(val, arr):
+def pathFinder(val, arr):
     x = len(arr)
     y = len(arr[0])
     # 1 Get starting point info from val
@@ -35,10 +57,10 @@ def _pathFinder(val, arr):
     s = Solver()
 
 
-def pathFinder(arr):
+def getUniqueVar(arr):
     toFind = []
-    toFindStart = []
-    toFindEnd = []
+    toFindStart = {}
+    toFindEnd = {}
 
     # 1 Find dimensions of arr
     x = len(arr)
@@ -48,32 +70,50 @@ def pathFinder(arr):
         for j in range(y):
             if arr[i][j] in db and arr[i][j] not in toFind:
                 toFind.append(arr[i][j])
-                toFindStart.append((arr[i][j], (i, j)))
+                # toFindStart.append((arr[i][j], (i, j)))
+                toFindStart[arr[i][j]] = (i, j)
             elif arr[i][j] in db and arr[i][j] in toFind:
-                toFindEnd.append((arr[i][j], (i, j)))
+                toFindEnd[arr[i][j]] = (i, j)
 
-    print(toFind)
     toFind.sort()
 
     if len(toFind) != len(toFindEnd) or len(toFind) != len(toFindEnd):
         print("Invalid entry, quitting")
         quit(1)
 
-    # 3 Find paths
-    variable(arr, toFind)
+    for i in toFind:
+        print(i, toFindStart[i], toFindEnd[i])
+
+    return toFind
 
 
 if __name__ == '__main__':
     print(sys.version)
+    # 1 Convert graph to SAT
 
-    arr1 = [['.' for i in range(5)] for j in range(5)]
-    arr1[0][0] = arr1[2][3] = 'a'
-    arr1[1][0] = arr1[3][4] = 'b'
-    arr1[0][3] = arr1[4][1] = 'c'
+    m = 6
+
+    arr1 = [['.' for i in range(m)] for j in range(m)]
+    arr1[1][0] = arr1[3][4] = 'a'
+    arr1[2][0] = arr1[4][5] = 'b'
+    arr1[1][4] = arr1[4][2] = 'c'
 
     for i in range(len(arr1)):
         for j in range(len(arr1[0])):
             print(arr1[i][j], " ", end='')
         print()
 
-    pathFinder(arr1)
+    print()
+
+    tp = edges(arr1)
+
+    pprint.pprint(tp)
+
+    """
+    for i in range(6):
+        for j in range(6):
+            print(str(i)+str(j)," ", tp[str(i)+str(j)])
+    """
+
+
+    #variable(arr1, getUniqueVar(arr1))
